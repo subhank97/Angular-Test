aed8abc4dece4435203ca0da726031f3c5cdf98c
Subhan Khan <subhank1997@gmail.com>
2023-07-18 19:05:39 -0400

    added api service to fetch cryptocurrency data and called it on homepage

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getCurrencyData(){
+    return this.http.get<any>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
+  }
+}
